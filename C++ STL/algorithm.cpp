033e3dc9424c21f07c738ba09208b815b5f43cee
kyrenewong <kyrenewong@163.com>
2017-10-28 22:20:32 +0800

    20171028
    20171028

---
+++ C++ STL/algorithm.cpp
@@ -0,0 +1,11 @@
+// 1. max() min() 和 abs()
+
+# include <stdio.h>
+# include <vector>
+using namespace std;
+int main(){
+	int x = 1, y = -2;
+	printf("%d %d\n", max(x,y), min(x,y));
+	printf("%d %d\n", abs(x), abs(y));
+	return 0；
+	}
