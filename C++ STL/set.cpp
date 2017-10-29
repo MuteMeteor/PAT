56991e6bdbf615528e223aab028331dc5e66d4b4
kyrenewong <kyrenewong@163.com>
2017-10-29 10:05:50 +0800

    20171029
    20171029

--- C++ STL/set.cpp
+++ C++ STL/set.cpp
@@ -51,61 +51,83 @@ int main(){
 
 // ===================================================
 // functions of common usage
-// (1) push_back()
-//     add an element x at the end of vector
-//     O(1)
-# include <stdio.h>
-# include <vector>
-using namespace std;
-int main(){
-	vector<int> vi;
-	for(int i = 1; i <= 3; i++){
-		vi.push_back(i);
-	}
-
-	for(int i = 0; i < vi.size(); i++){
-		printf("%d", vi[i]);
-	}
-
-	return 0;
-}
-// output: 123
+// (1) insert()
+//     insert x into the 'set'
+//     O(logN)   N is the number of the elements inside the 'set'
 
 // -----------------------------------------------------
-// (2) pop_back()
-//     delete an element x at the end of vector
-//     O(1)
-# include <stdio.h>
-# include <vector>
-using namespace std;
-int main(){
-	vector<int> vi;
-	for(int i = 1; i <= 3; i++){
-		vi.push_back(i);
-	}
-
-    vi.pop_back();
-	for(int i = 0; i < vi.size(); i++){
-		printf("%d", vi[i]);
-	}
-
-	return 0;
-}
-// output: 12
-
-// -----------------------------------------------------
-// (3) size()
+// (2) size()
 //     get the size of elements
 //     O(1)
 # include <stdio.h>
+# include <set>
+using namespace std;
+int main(){
+	set<int> st;
+	st.insert(2);
+	st.insert(5);
+	st.insert(4);
+	printf("%d", st.size());
+	return 0;
+}
+// output: 3
+
+
+// -----------------------------------------------------
+// (3) clear()
+//     clear the elements
+//     O(N)
+# include <stdio.h>
+# include <set>
+using namespace std;
+int main(){
+	set<int> st;
+	st.insert(2);
+	st.insert(5);
+	st.insert(4);
+	st.clear();
+	printf("%d", st.size());
+	return 0;
+}
+// output: 0
+// -----------------------------------------------------
+// (6) erase()
+//     1. delete one element on the position 'it'
+//     O(N)
+# include <stdio.h>
 # include <vector>
 using namespace std;
 int main(){
 	vector<int> vi;
-	for(int i = 1; i <= 3; i++){
+	for(int i = 1; i <= 5; i++){
 		vi.push_back(i);
 	}
-	printf("%d", vi.size());
+
+    vi.erase(vi.begin() +3);   // delete '4'
+	for(int i = 0; i < vi.size(); i++){
+		printf("%d", vi[i]);
+	}
+
 	return 0;
 }
-// output: 3
+// output:1235
+
+//     2. delete all elements in the section [first,last)
+//     O(N)
+# include <stdio.h>
+# include <vector>
+using namespace std;
+int main(){
+	vector<int> vi;
+	for(int i = 1; i <= 9; i++){
+		vi.push_back(i);
+	}
+
+    vi.erase(vi.begin() +1,vi.begin() +4);  
+	for(int i = 0; i < vi.size(); i++){
+		printf("%d", vi[i]);
+	}
+
+	return 0;
+}
+// output:156789
