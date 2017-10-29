6bafbb83abde9e4c76f8972c395991b872bf1745
kyrenewong <kyrenewong@163.com>
2017-10-29 09:27:37 +0800

    20171029
    20171029

---
+++ C++ STL/set.cpp
@@ -0,0 +1,21 @@
+// set 翻译为集合，是一个内部自动有序且不含重复元素的容器。
+
+// Define 
+set<typename> name;
+
+// examples
+set<int> name;
+set<double> name;
+set<char> name;
+set<node> name; // the type of struct 
+
+set<vector<int> > name;
+
+// array
+set<typename> Arrayname[arraysize];
+
+// examples
+set<int> a[100];
+
+
+// 
