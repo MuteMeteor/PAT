08bd767b1f026e2de156cc0e147a5ec4ba54545b
kyrenewong <kyrenewong@163.com>
2017-10-29 09:39:16 +0800

    20171029
    20171029

--- C++ STL/set.cpp
+++ C++ STL/set.cpp
@@ -18,4 +18,94 @@ set<typename> Arrayname[arraysize];
 set<int> a[100];
 
 
-// 
+
+
+// ================================================
+
+// visit of vector 
+// set 只能通过迭代器 （iterator） 访问
+
+set<typename> ::iterator it;
+set<int> ::iterator it;
+set<char> ::iterator it;
+
+# include <stdio.h>
+# include <set>
+using namespace std;
+int main(){
+	set<int> st;
+	st.insert(3);
+	st.insert(5);
+	st.insert(2);
+	st.insert(3);
+
+	for(set<int> ::iterator it = st.begin(); it != st.end(); it++){
+		printf("%d", *it);
+	}
+
+	return 0;
+}
+// output: 235
+
+
+
+// ===================================================
+// functions of common usage
+// (1) push_back()
+//     add an element x at the end of vector
+//     O(1)
+# include <stdio.h>
+# include <vector>
+using namespace std;
+int main(){
+	vector<int> vi;
+	for(int i = 1; i <= 3; i++){
+		vi.push_back(i);
+	}
+
+	for(int i = 0; i < vi.size(); i++){
+		printf("%d", vi[i]);
+	}
+
+	return 0;
+}
+// output: 123
+
+// -----------------------------------------------------
+// (2) pop_back()
+//     delete an element x at the end of vector
+//     O(1)
+# include <stdio.h>
+# include <vector>
+using namespace std;
+int main(){
+	vector<int> vi;
+	for(int i = 1; i <= 3; i++){
+		vi.push_back(i);
+	}
+
+    vi.pop_back();
+	for(int i = 0; i < vi.size(); i++){
+		printf("%d", vi[i]);
+	}
+
+	return 0;
+}
+// output: 12
+
+// -----------------------------------------------------
+// (3) size()
+//     get the size of elements
+//     O(1)
+# include <stdio.h>
+# include <vector>
+using namespace std;
+int main(){
+	vector<int> vi;
+	for(int i = 1; i <= 3; i++){
+		vi.push_back(i);
+	}
+	printf("%d", vi.size());
+	return 0;
+}
+// output: 3
